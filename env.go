6992b3d8e8a2329be350a771155ad4bac98208e1
Wayback Archiver <66856220+waybackarchiver@users.noreply.github.com>
2022-12-26 12:55:12 +0000

    Add func HasStdin and ReadStdin

--- env.go
+++ env.go
@@ -5,6 +5,7 @@
 package helper // import "github.com/wabarc/helper"
 
 import (
+	"bufio"
 	"os"
 )
 
@@ -13,4 +14,30 @@ func Unsetenv(envs ...string) {
 	for _, env := range envs {
 		os.Unsetenv(env)
 	}
+}
+
+// HasStdin determines if the user has piped input
+func HasStdin() bool {
+	stat, err := os.Stdin.Stat()
+	if err != nil {
+		return false
+	}
+
+	mode := stat.Mode()
+
+	isPipedFromChrDev := (mode & os.ModeCharDevice) == 0
+	isPipedFromFIFO := (mode & os.ModeNamedPipe) != 0
+
+	return isPipedFromChrDev || isPipedFromFIFO
+}
+
+// ReadStdin reads stdin to slice.
+func ReadStdin() (stdin []string) {
+	if HasStdin() {
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			stdin = append(stdin, scanner.Text())
+		}
+	}
+	return
 }
