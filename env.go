b5ec418536b78f6093f834b78ade069d81569978
Wayback Archiver <66856220+waybackarchiver@users.noreply.github.com>
2023-02-08 13:40:49 +0000

    Determine terminal

--- env.go
+++ env.go
@@ -7,7 +7,12 @@ package helper // import "github.com/wabarc/helper"
 import (
 	"bufio"
 	"os"
+	"strings"
+
+	"github.com/mattn/go-isatty"
 )
+
+const space = ` `
 
 // Unsetenv unsets given envs
 func Unsetenv(envs ...string) {
@@ -27,8 +32,10 @@ func HasStdin() bool {
 
 	isPipedFromChrDev := (mode & os.ModeCharDevice) == 0
 	isPipedFromFIFO := (mode & os.ModeNamedPipe) != 0
+	isTerminal := isatty.IsTerminal(os.Stdout.Fd()) ||
+		isatty.IsCygwinTerminal(os.Stdout.Fd())
 
-	return isPipedFromChrDev || isPipedFromFIFO
+	return isTerminal && (isPipedFromChrDev || isPipedFromFIFO)
 }
 
 // ReadStdin reads stdin to slice.
@@ -36,7 +43,7 @@ func ReadStdin() (stdin []string) {
 	if HasStdin() {
 		scanner := bufio.NewScanner(os.Stdin)
 		for scanner.Scan() {
-			stdin = append(stdin, scanner.Text())
+			stdin = append(stdin, strings.Split(scanner.Text(), space)...)
 		}
 	}
 	return
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.13
 require (
 	github.com/fortytw2/leaktest v1.3.0
 	github.com/kennygrant/sanitize v1.2.4
+	github.com/mattn/go-isatty v0.0.17
 	golang.org/x/net v0.0.0-20220708220712-1185a9018129
 	golang.org/x/text v0.3.7
 	mvdan.cc/xurls/v2 v2.4.0
